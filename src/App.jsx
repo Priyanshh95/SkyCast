b8ab0315feb2187a421ad36bde2ee218a4ea0803
Priyanshh95 <priyanshub243@gmail.com>
2025-07-17 19:30:35 +0530

    clean up starter template for custom weather app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>SkyCast Weather App</h1>
+      {/* Weather UI will go here */}
+    </div>
+  );
+}
+
+export default App; 
